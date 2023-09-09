590eee19693c29fe21b4529f23d3d7cf9bb01667
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2023-09-09 11:26:56 -0700

    Implemented Income Dto to prevent sending unused properties in AddIncome endpoint

---
+++ MonthBudget.API/Dtos/IncomeDto.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using MonthBudget.Data.Models;
+
+namespace MonthBudget.API.Dtos
+{
+    public class IncomeDto
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        [DefaultValue(1)]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
+        [DefaultValue("Employment")]
+        public string Source { get; set; } = null!;
+
+        [MaxLength(512)]
+        [DefaultValue("Paycheck 1")]
+        public string Note { get; set; } = null!;
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        [DefaultValue(1)]
+        public int AccountId { get; set; }
+
+        [Required]
+        [Range(1, double.MaxValue)]
+        [DefaultValue(1500)]
+        public double Amount { get; set; }
+
+        [Required]
+        public DateTime TransactionDate { get; set; }
+
+        public Income ConvertToIncome()
+        {
+            return new Income
+            {
+                UserId = UserId,
+                Source = Source,
+                Amount = Amount,
+                Note = Note,
+                AccountId = AccountId,
+                TransactionDate = TransactionDate
+            };
+        }
+    }
+}
--- MonthBudget.Data/Models/Income.cs
+++ MonthBudget.Data/Models/Income.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MonthBudget.Data.Models;
 
@@ -7,14 +7,27 @@ public partial class Income
 {
     public int Id { get; set; }
 
+    [Required]
+    [Range(1, int.MaxValue)]
     public int UserId { get; set; }
 
+    [Required]
+    [MaxLength(100)]
+    [MinLength(1)]
     public string Source { get; set; } = null!;
 
+    [MaxLength(512)]
     public string Note { get; set; } = null!;
 
+    [Required]
+    [Range(1, int.MaxValue)]
     public int AccountId { get; set; }
 
+    [Required]
+    [Range(1, double.MaxValue)]
+    public double Amount { get; set; }
+
+    [Required]
     public DateTime TransactionDate { get; set; }
 
     public DateTime CreatedOn { get; set; }
