db21c38e621959224f3e7c06091d93271f0931eb
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2023-09-12 14:36:36 -0700

    Defined RecurringIncome service contract

---
+++ MonthBudget.ServiceContracts/IRecurringIncomeService.cs
@@ -0,0 +1,11 @@
+﻿using MonthBudget.Data.Models;
+
+namespace MonthBudget.ServiceContracts
+{
+    public interface IRecurringIncomeService
+    {
+        Task<(RecurringIncome,List<Income>)> AddRecurringIncome(RecurringIncome recurringIncome);
+        Task<bool> RemoveRecurringIncome(int recurringIncomeId);
+        (List<RecurringIncome>,List<Income>) GetRecurringIncomes(int userId);
+    }
+}
