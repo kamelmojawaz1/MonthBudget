c0b6199b997e2521eeb789a70f21ec76fecfa456
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2023-09-12 14:35:26 -0700

    Reposityory

---
+++ MonthBudget.Data/Repositories/RecurringExpensesRepository.cs
@@ -0,0 +1,45 @@
+﻿using MonthBudget.Data.Models;
+
+namespace MonthBudget.Data.Repositories
+{
+    public class RecurringExpensesRepository
+    {
+        private readonly MonthBudgetDbContext _dbContext;
+
+        public RecurringExpensesRepository(MonthBudgetDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        public async Task<RecurringExpense> AddRecurringExpense(RecurringExpense expense)
+        {
+            await _dbContext.RecurringExpenses.AddAsync(expense);
+            await _dbContext.SaveChangesAsync();
+            return expense;
+        }
+
+        public async Task<bool> RemoveRecurringExpense(int expenseId)
+        {
+            var expense = _dbContext.RecurringExpenses.FirstOrDefault(e => e.Id == expenseId);
+            if (expense == null) return false;
+
+            expense.IsActive = false;
+            //expense.UpdatedOn = DateTime.Now;/**this assumes db and server are in the same timezone**/
+
+            var result = _dbContext.RecurringExpenses.Update(expense);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public RecurringExpense? GetById(int expenseId)
+        {
+            return _dbContext.RecurringExpenses.FirstOrDefault(e => e.Id == expenseId && e.IsActive == true);
+        }
+
+        public List<RecurringExpense> GetAll(int userId)
+        {
+            var expenses = _dbContext.RecurringExpenses.Where(e => e.UserId == userId && e.IsActive == true).ToList();
+            return expenses ?? new List<RecurringExpense>();
+        }
+    }
+}
---
+++ MonthBudget.Data/Repositories/RecurringIncomeRepository.cs
@@ -0,0 +1,45 @@
+﻿using MonthBudget.Data.Models;
+
+namespace MonthBudget.Data.Repositories
+{
+    public class RecurringIncomeRepository
+    {
+        private readonly MonthBudgetDbContext _dbContext;
+
+        public RecurringIncomeRepository(MonthBudgetDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        public async Task<RecurringIncome> AddRecurringIncome(RecurringIncome income)
+        {
+            await _dbContext.RecurringIncomes.AddAsync(income);
+            await _dbContext.SaveChangesAsync();
+            return income;
+        }
+
+        public async Task<bool> RemoveRecurringIncome(int incomeId)
+        {
+            var income = _dbContext.RecurringIncomes.FirstOrDefault(e => e.Id == incomeId);
+            if (income == null) return false;
+
+            income.IsActive = false;
+            //expense.UpdatedOn = DateTime.Now;/**this assumes db and server are in the same timezone**/
+
+            var result = _dbContext.RecurringIncomes.Update(income);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public RecurringIncome? GetById(int incomeId)
+        {
+            return _dbContext.RecurringIncomes.FirstOrDefault(e => e.Id == incomeId && e.IsActive == true);
+        }
+
+        public List<RecurringIncome> GetAll(int userId)
+        {
+            var incomes = _dbContext.RecurringIncomes.Where(e => e.UserId == userId && e.IsActive == true).ToList();
+            return incomes ?? new List<RecurringIncome>();
+        }
+    }
+}
