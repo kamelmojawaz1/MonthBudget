fd8f8f4b954c1822619eb464dc3c10770b8fe609
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2023-09-12 18:16:39 -0700

    Defined Account service contract

---
+++ MonthBudget.ServiceContracts/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using MonthBudget.Data.Models;
+
+namespace MonthBudget.ServiceContracts
+{
+    public interface IAccountService
+    {
+        Task<Account> AddAccount(Account account);
+        Task<bool> RemoveAccount(int accountId);
+        List<Account> GetAccounts(int userId);
+    }
+}
