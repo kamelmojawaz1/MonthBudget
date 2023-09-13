f7b193eef306093a8e4ecffb4f735fb3b4846aa1
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2023-09-12 18:29:49 -0700

    Created Account Dto

---
+++ MonthBudget.API/Dtos/AccountDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using MonthBudget.Data.Models;
+using System.Text.Json;
+
+namespace MonthBudget.API.Dtos
+{
+    public class AccountDto
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        [DefaultValue(1)]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
+        [DefaultValue("Checking Account")]
+        public string AccountName { get; set; } = null!;
+
+        public Account ConvertToAccount()
+        {
+            return new Account
+            {
+                UserId = UserId,
+                AccountName = AccountName
+            };
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<AccountDto>(this);
+        }
+    }
+}
