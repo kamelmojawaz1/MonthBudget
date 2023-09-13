f51534a288d56d0c850a465850a5346dd2c34b29
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2023-09-12 18:29:38 -0700

    Added validation attributes

--- MonthBudget.Data/Models/Account.cs
+++ MonthBudget.Data/Models/Account.cs
@@ -1,11 +1,19 @@
-﻿namespace MonthBudget.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MonthBudget.Data.Models;
 
 public partial class Account
 {
     public int Id { get; set; }
 
+
+    [Required]
+    [Range(1, int.MaxValue)]
     public int UserId { get; set; }
 
+    [Required]
+    [MaxLength(100)]
+    [MinLength(1)]
     public string AccountName { get; set; } = null!;
 
     public bool? IsActive { get; set; }
