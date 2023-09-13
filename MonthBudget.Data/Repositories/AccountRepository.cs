20a8ece72ec375b6d38d7a2fe732fd619e591da5
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2023-09-12 18:09:38 -0700

    Implemented Account Repository

---
+++ MonthBudget.Data/Repositories/AccountRepository.cs
@@ -0,0 +1,40 @@
+﻿using MonthBudget.Data.Models;
+
+namespace MonthBudget.Data.Repositories
+{
+    public class AccountRepository
+    {
+        private readonly MonthBudgetDbContext _dbContext;
+
+        public AccountRepository(MonthBudgetDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        public async Task<Account> AddAccount(Account account)
+        {
+            await _dbContext.Accounts.AddAsync(account);
+            await _dbContext.SaveChangesAsync();
+            return account;
+        }
+
+        public async Task<bool> RemoveAccount(int accountId)
+        {
+            var account = _dbContext.Accounts.FirstOrDefault(i => i.Id == accountId);
+            if (account == null) return false;
+
+            account.IsActive = false;
+
+            var result = _dbContext.Accounts.Update(account);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public List<Account> GetAll(int userId)
+        {
+            var accounts = _dbContext.Accounts.Where(i => i.UserId == userId && i.IsActive == true).ToList();
+            return accounts ?? new List<Account>();
+        }
+
+    }
+}
